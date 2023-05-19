812aa279e07aa2075a1bf8397f1ff16f5071d97f
gabriels5g <gabrielspxls@gmail.com>
2023-05-19 01:35:44 -0300

    advancing on the back-end and front-nd

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,19 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async (req, res) => {
-  const users = await prisma.users.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+
+app.register(memoriesRoutes)
 
 app
   .listen({
-    port: 3000,
+    port: 3333,
   })
   .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3000')
+    console.log('🚀 HTTP server running on http://localhost:3333')
   })
