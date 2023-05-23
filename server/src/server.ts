469a724bd79ecc5e82b44f53f6262342e7ac31d6
gabriels5g <gabrielspxls@gmail.com>
2023-05-23 01:38:00 -0300

    starting authentication

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 
@@ -8,6 +9,10 @@ const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+
+app.register(jwt, {
+  secret: 'hndqwy8ey82g178ye0919u271g48912vr81',
 })
 
 app.register(memoriesRoutes)
